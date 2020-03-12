7eaa897f7445fccad8f7d257271aeb66ffb72afb
leooooliu <leooooliu@outlook.com>
2020-03-12 18:30:11 -0500

    'm'

--- src/data_collection.py
+++ src/data_collection.py
@@ -20,8 +20,8 @@ logging.basicConfig(level=logging.info)
 
 def fetch_data(path,sample = True):
     """fetch json data from a path """
-    correct_cols_name =['country', 'customer_id', 'day', 'invoice', 'month', 'price',
-       'stream_id', 'times_viewed', 'year'].sort()
+    correct_cols_name =sorted(['country', 'customer_id', 'day', 'invoice', 'month', 'price',
+       'stream_id', 'times_viewed', 'year'])
     all_json_data = []
     for i in glob.glob(os.path.join(path,'*')):
         with open(i) as f:
@@ -33,8 +33,12 @@ def fetch_data(path,sample = True):
                 data[idx]['price'] = data[idx].pop('total_price')
                 logging.debug(f'key_name total_price  is not standardized,'+ 
                                 ' and has been changed to price in file {i}')
-            if  list(item.keys()).sort() != correct_cols_name:
-                logging.warning(f'key name of element {idx} in file {i} is {item.keys().__str__())}' +
+            if 'StreamID' in item.keys():
+                data[idx]['stream_id'] = data[idx].pop('StreamID')
+            if 'TimesViewed' in item.keys():
+                data[idx]['times_viewed'] = data[idx].pop('TimesViewed')
+            if  sorted(list(item.keys())) != correct_cols_name:
+                logging.warning(f'key name of element {idx} in file {i} is {item.keys().__str__()}' +
                                 'and it has been removed from file')
                 unstandardized_element.append(idx)
         data = list( data[i] for i in range(len(data)) if i not in unstandardized_element)
@@ -42,6 +46,6 @@ def fetch_data(path,sample = True):
         if sample:break
     return pd.DataFrame(all_json_data)
 
-
+#all_keys = fetch_data('D:\\github\\ai-workflow-capstone\\test\\',False)
 #if __name__
 #all_keys = fetch_data(prod_data,False)
