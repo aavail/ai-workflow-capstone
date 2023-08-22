940f9bb78299564fde05d5ba5ccc26168ee8790c
aenache99 <alexenache71@gmail.com>
2023-08-22 20:57:47 +0300

    first commit

--- solution_guidance/model.py
+++ solution_guidance/model.py
@@ -1,4 +1,4 @@
-import time,os,re,csv,sys,uuid,joblib
+import time, os, re, csv, sys, uuid, joblib
 from datetime import date
 from collections import defaultdict
 import numpy as np
@@ -13,12 +13,13 @@ from sklearn.pipeline import Pipeline
 from logger import update_predict_log, update_train_log
 from cslib import fetch_ts, engineer_features
 
-## model specific variables (iterate the version and note with each change)
+# model specific variables (iterate the version and note with each change)
 MODEL_DIR = "models"
 MODEL_VERSION = 0.1
 MODEL_VERSION_NOTE = "supervised learing model for time-series"
 
-def _model_train(df,tag,test=False):
+
+def _model_train(df, tag, test=False):
     """
     example funtion to train model
     
@@ -28,68 +29,67 @@ def _model_train(df,tag,test=False):
 
     """
 
-
-    ## start timer for runtime
+    # start timer for runtime
     time_start = time.time()
-    
-    X,y,dates = engineer_features(df)
+
+    X, y, dates = engineer_features(df)
 
     if test:
         n_samples = int(np.round(0.3 * X.shape[0]))
-        subset_indices = np.random.choice(np.arange(X.shape[0]),n_samples,
+        subset_indices = np.random.choice(np.arange(X.shape[0]), n_samples,
                                           replace=False).astype(int)
-        mask = np.in1d(np.arange(y.size),subset_indices)
-        y=y[mask]
-        X=X[mask]
-        dates=dates[mask]
-        
-    ## Perform a train-test split
+        mask = np.in1d(np.arange(y.size), subset_indices)
+        y = y[mask]
+        X = X[mask]
+        dates = dates[mask]
+
+    # Perform a train-test split
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25,
                                                         shuffle=True, random_state=42)
-    ## train a random forest model
+    # train a random forest model
     param_grid_rf = {
-    'rf__criterion': ['mse','mae'],
-    'rf__n_estimators': [10,15,20,25]
+        'rf__criterion': ['mse', 'mae'],
+        'rf__n_estimators': [10, 15, 20, 25]
     }
 
     pipe_rf = Pipeline(steps=[('scaler', StandardScaler()),
                               ('rf', RandomForestRegressor())])
-    
+
     grid = GridSearchCV(pipe_rf, param_grid=param_grid_rf, cv=5, iid=False, n_jobs=-1)
     grid.fit(X_train, y_train)
     y_pred = grid.predict(X_test)
-    eval_rmse =  round(np.sqrt(mean_squared_error(y_test,y_pred)))
-    
-    ## retrain using all data
+    eval_rmse = round(np.sqrt(mean_squared_error(y_test, y_pred)))
+
+    # retrain using all data
     grid.fit(X, y)
-    model_name = re.sub("\.","_",str(MODEL_VERSION))
+    model_name = re.sub("\.", "_", str(MODEL_VERSION))
     if test:
         saved_model = os.path.join(MODEL_DIR,
-                                   "test-{}-{}.joblib".format(tag,model_name))
+                                   "test-{}-{}.joblib".format(tag, model_name))
         print("... saving test version of model: {}".format(saved_model))
     else:
         saved_model = os.path.join(MODEL_DIR,
-                                   "sl-{}-{}.joblib".format(tag,model_name))
+                                   "sl-{}-{}.joblib".format(tag, model_name))
         print("... saving model: {}".format(saved_model))
-        
-    joblib.dump(grid,saved_model)
 
-    m, s = divmod(time.time()-time_start, 60)
+    joblib.dump(grid, saved_model)
+
+    m, s = divmod(time.time() - time_start, 60)
     h, m = divmod(m, 60)
-    runtime = "%03d:%02d:%02d"%(h, m, s)
+    runtime = "%03d:%02d:%02d" % (h, m, s)
 
     ## update log
-    update_train_log(tag,(str(dates[0]),str(dates[-1])),{'rmse':eval_rmse},runtime,
-                     MODEL_VERSION, MODEL_VERSION_NOTE,test=True)
-  
+    update_train_log(tag, (str(dates[0]), str(dates[-1])), {'rmse': eval_rmse}, runtime,
+                     MODEL_VERSION, MODEL_VERSION_NOTE, test=True)
 
-def model_train(data_dir,test=False):
+
+def model_train(data_dir, test=False):
     """
     funtion to train model given a df
     
     'mode' -  can be used to subset data essentially simulating a train
     """
-    
+
     if not os.path.isdir(MODEL_DIR):
         os.mkdir(MODEL_DIR)
 
@@ -97,19 +97,20 @@ def model_train(data_dir,test=False):
         print("... test flag on")
         print("...... subseting data")
         print("...... subseting countries")
-        
+
     ## fetch time-series formatted data
     ts_data = fetch_ts(data_dir)
 
     ## train a different model for each data sets
-    for country,df in ts_data.items():
-        
-        if test and country not in ['all','united_kingdom']:
+    for country, df in ts_data.items():
+
+        if test and country not in ['all', 'united_kingdom']:
             continue
-        
-        _model_train(df,country,test=test)
-    
-def model_load(prefix='sl',data_dir=None,training=True):
+
+        _model_train(df, country, test=test)
+
+
+def model_load(prefix='sl', data_dir=None, training=True):
     """
     example funtion to load model
     
@@ -117,53 +118,54 @@ def model_load(prefix='sl',data_dir=None,training=True):
     """
 
     if not data_dir:
-        data_dir = os.path.join("..","data","cs-train")
-    
-    models = [f for f in os.listdir(os.path.join(".","models")) if re.search("sl",f)]
+        data_dir = os.path.join("..", "data", "cs-train")
+
+    models = [f for f in os.listdir(os.path.join(".", "models")) if re.search("sl", f)]
 
     if len(models) == 0:
         raise Exception("Models with prefix '{}' cannot be found did you train?".format(prefix))
 
     all_models = {}
     for model in models:
-        all_models[re.split("-",model)[1]] = joblib.load(os.path.join(".","models",model))
+        all_models[re.split("-", model)[1]] = joblib.load(os.path.join(".", "models", model))
 
-    ## load data
+    # load data
     ts_data = fetch_ts(data_dir)
     all_data = {}
     for country, df in ts_data.items():
-        X,y,dates = engineer_features(df,training=training)
+        X, y, dates = engineer_features(df, training=training)
         dates = np.array([str(d) for d in dates])
-        all_data[country] = {"X":X,"y":y,"dates": dates}
-        
-    return(all_data, all_models)
+        all_data[country] = {"X": X, "y": y, "dates": dates}
 
-def model_predict(country,year,month,day,all_models=None,test=False):
+    return (all_data, all_models)
+
+
+def model_predict(country, year, month, day, all_models=None, test=False):
     """
     example funtion to predict from model
     """
 
-    ## start timer for runtime
+    # start timer for runtime
     time_start = time.time()
 
-    ## load model if needed
+    # load model if needed
     if not all_models:
-        all_data,all_models = model_load(training=False)
-    
-    ## input checks
+        all_data, all_models = model_load(training=False)
+
+    # input checks
     if country not in all_models.keys():
         raise Exception("ERROR (model_predict) - model for country '{}' could not be found".format(country))
 
-    for d in [year,month,day]:
-        if re.search("\D",d):
+    for d in [year, month, day]:
+        if re.search("\D", d):
             raise Exception("ERROR (model_predict) - invalid year, month or day")
-    
-    ## load data
+
+    # load data
     model = all_models[country]
     data = all_data[country]
 
-    ## check date
-    target_date = "{}-{}-{}".format(year,str(month).zfill(2),str(day).zfill(2))
+    # check date
+    target_date = "{}-{}-{}".format(year, str(month).zfill(2), str(day).zfill(2))
     print(target_date)
 
     if target_date not in data['dates']:
@@ -172,49 +174,48 @@ def model_predict(country,year,month,day,all_models=None,test=False):
                                                                                     data['dates'][-1]))
     date_indx = np.where(data['dates'] == target_date)[0][0]
     query = data['X'].iloc[[date_indx]]
-    
-    ## sainty check
+
+    # sanity check
     if data['dates'].shape[0] != data['X'].shape[0]:
         raise Exception("ERROR (model_predict) - dimensions mismatch")
 
-    ## make prediction and gather data for log entry
+    # make prediction and gather data for log entry
     y_pred = model.predict(query)
     y_proba = None
     if 'predict_proba' in dir(model) and 'probability' in dir(model):
         if model.probability == True:
             y_proba = model.predict_proba(query)
 
-
-    m, s = divmod(time.time()-time_start, 60)
+    m, s = divmod(time.time() - time_start, 60)
     h, m = divmod(m, 60)
-    runtime = "%03d:%02d:%02d"%(h, m, s)
+    runtime = "%03d:%02d:%02d" % (h, m, s)
 
-    ## update predict log
-    update_predict_log(country,y_pred,y_proba,target_date,
+    # update predict log
+    update_predict_log(country, y_pred, y_proba, target_date,
                        runtime, MODEL_VERSION, test=test)
-    
-    return({'y_pred':y_pred,'y_proba':y_proba})
+
+    return ({'y_pred': y_pred, 'y_proba': y_proba})
+
 
 if __name__ == "__main__":
-
     """
     basic test procedure for model.py
     """
 
-    ## train the model
+    # train the model
     print("TRAINING MODELS")
-    data_dir = os.path.join("..","data","cs-train")
-    model_train(data_dir,test=True)
+    data_dir = os.path.join("..", "data", "cs-train")
+    model_train(data_dir, test=True)
 
-    ## load the model
+    # load the model
     print("LOADING MODELS")
     all_data, all_models = model_load()
-    print("... models loaded: ",",".join(all_models.keys()))
+    print("... models loaded: ", ",".join(all_models.keys()))
 
-    ## test predict
-    country='all'
-    year='2018'
-    month='01'
-    day='05'
-    result = model_predict(country,year,month,day)
+    # test predict
+    country = 'all'
+    year = '2018'
+    month = '01'
+    day = '05'
+    result = model_predict(country, year, month, day)
     print(result)
